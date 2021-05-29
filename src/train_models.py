67726caecbd6104535616aa654c05336b7575bca
Fiorellaps <61502415+Fiorellaps@users.noreply.github.com>
2021-05-29 09:18:09 +0200

    Update train_models.py

--- src/train_models.py
+++ src/train_models.py
@@ -4,26 +4,21 @@ import random
 import itertools
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier,AdaBoostClassifier,GradientBoostingClassifier
-from sklearn.datasets import make_moons
-from keras.preprocessing import text, sequence
+from keras.preprocessing import sequence
 from keras.preprocessing.text import Tokenizer
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import StratifiedShuffleSplit
-import joblib
 from  tensorflow.keras.models import Sequential
 from  tensorflow.keras.layers import Dense, Conv1D, MaxPooling1D, Flatten, LSTM, Embedding, Dropout, Bidirectional, GRU
 from tensorflow.keras.utils import plot_model
-from tensorflow.keras.callbacks import TensorBoard
-from sklearn.model_selection import StratifiedKFold
 from keras.wrappers.scikit_learn import KerasClassifier
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.model_selection import KFold 
 
+
+#output_text = open ('./output_text.txt',)
 ######## Read data ########
 df = pd.read_csv('../resources/input/full_pair_data.csv')
 df.reset_index()
@@ -37,7 +32,7 @@ random.seed(1024)
 stratified_split = StratifiedShuffleSplit(n_splits=2, test_size=0.1, random_state=0)
 for x, y in stratified_split.split(df, df['Class']):
     stratified_df = df.iloc[y]
-
+stratified_df = df
 sns.countplot(stratified_df['Class'])
 plt.savefig("./output_images/class_distribution.png")
 
@@ -70,38 +65,27 @@ X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, stratif
 
 ######## Functions ########
 
-# Train and test model
-def evaluate_model(model, X_train = X_train, X_test = X_test, y_train = y_train, y_test = y_test):
-    model.fit(X_train, y_train)
-    prediction = model.predict(X_test)
-    accuracy_value= accuracy_score(y_test, prediction)
-    confusion_matrix_model = confusion_matrix(y_test, prediction)
-    model_name = str(model.__class__.__name__)
-    models_names.append(model_name)
-    models_scores.append(accuracy_value)
-    plot_confusion_matrix (confusion_matrix_model,classes, model_name)
-    #output_text.write(str(model) + ": Accuracy = " + str(accuracy_value))
-    print(str(model) + ": Accuracy = " + str(accuracy_value))
-    return accuracy_value
-
-
 # Hyperparemeters tuning
 def hiperparametrization_grid_search(model, prameters_grid, X_train , X_test , y_train , y_test ):
-    model_grid_search = GridSearchCV(estimator = model, param_grid = prameters_grid, cv=10, n_jobs = -1)
+    # Apply grid search with 10 CV
+    model_grid_search = GridSearchCV(estimator = model, param_grid = prameters_grid, cv = 10, n_jobs = -1)
+    # Train models with given parameters
     model_grid_search.fit(X_train, y_train)
-    model_grid_search.best_params_
+    # Select the best estimator
     best_model = model_grid_search.best_estimator_
+    # Predict output
     prediction = best_model.predict(X_test)
+    # Compute accuracy
     accuracy_value= accuracy_score(y_test, prediction)
+    # Plot confusion matrix
     confusion_matrix_model = confusion_matrix(y_test, prediction)
+
     model_name = str(model.__class__.__name__)
     models_names.append(model_name)
     models_scores.append(accuracy_value)
     plot_confusion_matrix (confusion_matrix_model,classes, model_name)
-    #output_text.write(str(best_model) + ": Accuracy = " + str(accuracy_value))
     print(str(best_model) + ": Accuracy = " + str(accuracy_value))
     print(model_grid_search.best_params_)
-    #joblib.dump(best_model, './model/model_' + model_name + '.pkl') 
     return best_model
 
 # Compute normalized confusion matrix
@@ -109,7 +93,6 @@ def hiperparametrization_grid_search(model, prameters_grid, X_train , X_test , y
 def plot_confusion_matrix(confusion_matrix, classes, model_name, title = 'Confusion matrix', cmap = plt.cm.Blues):
     confusion_matrix = confusion_matrix.astype('float') / confusion_matrix.sum(axis=1)[:, np.newaxis]
     print(confusion_matrix) 
-    #output_text.write(confusion_matrix)
     plt.figure()
     plt.imshow(confusion_matrix, interpolation='nearest', cmap=cmap)
     plt.title(title + ' ' + model_name)
@@ -129,30 +112,23 @@ def plot_confusion_matrix(confusion_matrix, classes, model_name, title = 'Confus
     plt.savefig('./output_images/confusion_matrix_' + model_name + '.png')
 
 ######## Create models ########
-'''
+
 #-------- Decision Tree model --------#
 # Create model
 decision_tree_model = DecisionTreeClassifier(min_samples_leaf= 3)
-#decision_tree_model = DecisionTreeClassifier(criterion = 'entropy', max_depth = 10, min_samples_leaf= 3)
-
-#accuracy_decision_tree_model = evaluate_model(decision_tree_model, X_train, X_test, y_train, y_test) 
-#Define parameters
+# Define parameters
 parameter_grid_decision_tree = {
     "max_depth": range(1 ,10, 2),
     "min_samples_split": range(2, 10, 2),
     "criterion": ["gini", "entropy"]
 }
-
 # Grid Search Tuning
 decision_tree_best_model = hiperparametrization_grid_search(decision_tree_model, parameter_grid_decision_tree, X_train, X_test, y_train, y_test)
 
 #-------- Random Forest model --------#
 # Create model
 random_forest_model = RandomForestClassifier(n_estimators = 100, max_features = 'auto', random_state = 0, min_samples_leaf = 5, min_samples_split = 12)
-#random_forest_model = RandomForestClassifier(n_estimators = 300, max_depth = 30, criterion = 'entropy', max_features = 'auto', random_state = 0, min_samples_leaf = 5, min_samples_split = 12)
-
-#accuracy_random_forest_model = evaluate_model(random_forest_model, X_train, X_test, y_train, y_test) 
-#Define parameters
+# Define parameters
 parameter_grid_random_forest = {
     "criterion": ['gini', 'entropy'],
     "n_estimators": [200, 300, 400],
@@ -164,26 +140,20 @@ random_forest_best_model = hiperparametrization_grid_search(random_forest_model,
 #-------- Gradient Boosting model --------#
 # Create model
 gradient_boosting_model = GradientBoostingClassifier(min_samples_leaf = 60, subsample = 0.85, random_state = 10, max_features = 7)
-#gradient_boosting_model = GradientBoostingClassifiern_estimators(n_estimators = 300, learning_rate = 0.2, max_depth = 7, min_samples_split = , 600, min_samples_leaf = 60, subsample = 0.85, random_state = 10, max_features = 7)
-#accuracy_gradient_boosting_model = evaluate_model(gradient_boosting_model, X_train, X_test, y_train, y_test) 
-#Define parameters
+# Define parameters
 parameter_grid_gradient_boosting = {
     "n_estimators": range(100, 400, 100),
     "max_depth": range(2, 9, 2), 
     "min_samples_split": range(400, 700, 100),
     "learning_rate": [0.1, 0.2]
 }
-
 # Grid Search Tuning
 gradient_boosting_best_model = hiperparametrization_grid_search(gradient_boosting_model, parameter_grid_gradient_boosting, X_train, X_test, y_train, y_test)
 
 #-------- AdaBoost model --------#
 # Create model
 adaboost_model = AdaBoostClassifier(n_estimators = 300, learning_rate = 0.3)
-#adaboost_model = AdaBoostClassifier(n_estimators = 100)
-
-#accuracy_adabost_model = evaluate_model(adaboost_model, X_train, X_test, y_train, y_test) 
-#Define parameters
+# Define parameters
 parameter_grid_adaboost = {
     "n_estimators": range(100, 400, 100),
     "learning_rate": [0.2, 0.3, 0.4]
@@ -194,9 +164,7 @@ adaboost_best_model = hiperparametrization_grid_search(adaboost_model, parameter
 #-------- K-nearest Neighbors model --------#
 # Create model
 knn_model = KNeighborsClassifier(n_neighbors = 7)
-#knn_model = KNeighborsClassifier(n_neighbors = 3 , leaf_size = 30, p = 2)
-#evaluate_model(knn_model, X_train, X_test, y_train, y_test) 
-#Define parameters
+# Define parameters
 parameter_grid_knn = {
 	"leaf_size":range(1, 50, 10),
 	"n_neighbors": range(2, 16, 3),
@@ -206,9 +174,9 @@ parameter_grid_knn = {
 knn_best_model = hiperparametrization_grid_search(knn_model, parameter_grid_knn, X_train, X_test, y_train, y_test)
 
 
-#-------- CNN + BiLSTM model --------#
+#-------- CNN + BiLSTM --------#
 
-# create Sequential model 
+# Create Sequential model 
 def create_CNN_BiLSTM(optimizer='nadam'):
     embedding_dim = 21
     top_classes = 4
@@ -224,27 +192,24 @@ def create_CNN_BiLSTM(optimizer='nadam'):
     model_CNN_BiLSTM.add(Dense(128, activation='selu'))
     model_CNN_BiLSTM.add(Dense(top_classes, activation='softmax'))
     model_CNN_BiLSTM.compile(loss = 'sparse_categorical_crossentropy', optimizer = optimizer, metrics = ['accuracy'])
+    # Function to plot model architecture
     #plot_model(model_CNN_BiLSTM, to_file = './output_images/model_CNN_BiLSTM.png', show_shapes = False,
     #show_layer_names = True, rankdir = 'TB', expand_nested = False, dpi = 96)
     return model_CNN_BiLSTM
-# Plot model
 
-
-# Train model
-#kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
-#model_CNN_BiLSTM.fit(X_train, y_train, epochs = 10, batch_size = 512, callbacks = callbacks)
+# Initialize model
 model_CNN_BiLSTM = KerasClassifier(build_fn = create_CNN_BiLSTM, verbose = 0)
+# Define parameters
 parameter_grid_CNN_BiLSTM = {
 	"epochs" : [5, 10, 15],
     "batch_size" : [512, 1024],
     "optimizer" : ['rmsprop', 'nadam']
 }
-
+# Grid Search Tuning
 CNN_BiLSTM_best_model = hiperparametrization_grid_search(model_CNN_BiLSTM, parameter_grid_CNN_BiLSTM, X_train, X_test, y_train, y_test) 
 
-
-'''
-#-------- CNN + model --------#
+#-------- CNN --------#
+# Create Sequential model 
 def create_CNN(optimizer='nadam'):
     embedding_dim = 21
     top_classes = 4
@@ -258,21 +223,49 @@ def create_CNN(optimizer='nadam'):
     model_CNN.add(Dense(top_classes, activation='softmax'))
     model_CNN.compile(loss = 'sparse_categorical_crossentropy', optimizer = optimizer, metrics = ['accuracy'])
     return model_CNN
+# Initialize model
 model_CNN = KerasClassifier(build_fn = create_CNN, verbose = 0)
+# Define parameters
 parameter_grid_CNN = {
 	"epochs" : [5, 10, 15],
     "batch_size" : [512, 1024],
     "optimizer" : ['adadelta', 'nadam']
 }
-#CNN_best_model = hiperparametrization_grid_search(model_CNN, parameter_grid_CNN, X_train, X_test, y_train, y_test) 
+# Grid Search Tuning
+CNN_best_model = hiperparametrization_grid_search(model_CNN, parameter_grid_CNN, X_train, X_test, y_train, y_test) 
 
-model_CNN.fit(X_train, y_train, epochs = 5, batch_size = 512)
-prediction = model_CNN.predict(X_test)
-accuracy_value= accuracy_score(y_test, prediction)
-confusion_matrix_model = confusion_matrix(y_test, prediction)
-#model_name = str(model_CNN.__class__.__name__)
-#models_names.append(model_name)
-models_scores.append(accuracy_value)
-plot_confusion_matrix (confusion_matrix_model,classes, 'CNN')
-#output_text.write(str(model) + ": Accuracy = " + str(accuracy_value))
-print(str(model_CNN) + ": Accuracy = " + str(accuracy_value))
+
+#---------- Plot final statistics to compare models (optional) ---------#
+'''
+plt.figure()
+models_names = ['DT', 'RF', 'GB','AdaBoost', 'CNN-BiLSTM', 'CNN']
+models_scores = [0.711, 0.870, 0.864, 0.674, 0.883, 0.861]
+plt.title("Final Models Scores",fontsize=22)
+plt.ylabel("ACC",fontsize=18)
+plt.xlabel("Model",fontsize=18)
+plt.ylim(0.6,0.89)
+plt.bar(models_names,models_scores, color = ['b', 'r', 'g', 'y', 'k', 'orange'])
+plt.savefig("./output_images/final_models_acc.png")
+
+dt = [0.636, 0.6996, 0.701, 0.711]
+knn = [0.730, 0.802, 0.828, 0]
+rf = [0.724, 0.794, 0.826, 0.870]
+gb = [0.750, 0.812, 0.831, 0.864]
+ab = [0.656, 0.664, 0.674, 0.676]
+cnnbilstm = [0.232, 0.717, 0.783, 0.883]
+cnn = [0.589, 0.756, 0.796, 0.861]
+
+plt.figure()
+plt.title("Models Progression",fontsize=22)
+plt.ylabel("ACC",fontsize=18)
+plt.ylim(0.40,0.89)
+plt.plot(dt, linestyle='-', label= 'dt')  # solid
+plt.plot(knn, linestyle='--', label= 'knn') # dashed
+plt.plot(rf, linestyle='-.', label='rf') # dashdot
+plt.plot(gb, linestyle=':',label='gb')
+plt.plot(ab, linestyle='-', label='adaboost')  # solid
+plt.plot(cnnbilstm, linestyle='--', label='cnnbilstm') # dashed
+plt.plot(cnn, linestyle='-.',label='cnn') # dashdot
+plt.legend()
+plt.savefig("./output_images/models_progression_acc.png")
+'''
